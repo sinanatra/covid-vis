0fab372a3e56eefb5e80e8f99e5c794a0a52379d
Giacomo Nanni <giacomo.nanni@chialab.it>
2020-03-24 12:21:37 +0100

    feat: cloropeth map

--- assets/app.js
+++ assets/app.js
@@ -1,32 +1,38 @@
 
-const margin = {
-    top: 50,
-    right: 50,
-    bottom: 50,
-    left: 50
-},
-    width = window.innerWidth - margin.left - margin.right,
-    height = window.innerHeight - margin.top - margin.bottom;
+const height = 600;
+const width = 900;
+let projection = [];
+let path = "";
 
-const projection = globalThis.d3.geoMercator()
-    .center([11, 44])
-    .scale(3200)
-    .precision(0.1)
-    .translate([width / 2, height / 2.5]);
+let italy = {};
 
-const path = globalThis.d3.geoPath()
-    .projection(projection)
+const color = globalThis.d3.scaleThreshold()
+    .domain([100, 1000, 2000, 3000, 4000, 5000, 6000])
+    .range(globalThis.d3.schemeBuPu[8]);
 
 async function loadMap() {
+    italy = await globalThis.d3.json('assets/json/province.geojson');
+
+    projection = globalThis.d3.geoEquirectangular()
+        .center([11, 44])
+        .scale(3200)
+        .precision(0.1)
+        .fitSize([width, height], italy);
+
+    path = globalThis.d3.geoPath()
+        .projection(projection)
 
     const svg = globalThis.d3.select('#map')
         .append('div')
         .attr('id', 'svg')
         .append('svg')
         .attr("width", width)
-        .attr("height", height)
+        .attr("height", height);
 
-    const italy = await globalThis.d3.json('assets/json/italy.geojson');
+ 
+
+    globalThis.d3.geoIdentity().fitSize([width, height], italy)
+
 
     const g = svg.append('g');
 
@@ -44,6 +50,7 @@ function loadData(data) {
 }
 
 async function loadMarkers(features, data) {
+
     const g = globalThis.d3.select('g')
 
     let filteredData = []
@@ -56,15 +63,82 @@ async function loadMarkers(features, data) {
 
     console.log(filteredData)
 
-    g.selectAll('circle')
-        .data(filteredData)
+    // Draw the legend
+    const format = globalThis.d3.format("d")
+
+    const legend = g => {
+        const x = globalThis.d3.scaleLinear()
+            .domain(globalThis.d3.extent(color.domain()))
+            .rangeRound([0, 250]);
+      
+        g.selectAll("rect")
+          .data(color.range().map(d => color.invertExtent(d)))
+          .join("rect")
+            .attr("height", 8)
+            .attr("x", d => x(d[0]))
+            .attr("width", d => x(d[1]) - x(d[0]))
+            .attr("fill", d => color(d[0]));
+      
+        g.append("text")
+            .attr("class", "caption")
+            .attr("x", x.range()[0])
+            .attr("y", -6)
+            .attr("fill", "#000")
+            .attr("text-anchor", "start")
+            .attr("font-weight", "bold")
+            .text("Affected by covid-19");
+        
+        g.call(globalThis.d3.axisBottom(x)
+            .tickSize(13)
+            .tickFormat(format)
+            .tickValues(color.range().slice(1).map(d => color.invertExtent(d)[0])))
+          .select(".domain")
+            .remove();
+      }
+      
+      g.append("g")
+        .attr("transform", "translate(600,40)")
+        .call(legend);
+
+    //  Merge geojson and filteredData
+    for (let i = 0; i < filteredData.length; i++) {
+        let dataState = filteredData[i].denominazione_provincia;
+
+        let dataValue = parseFloat(filteredData[i].totale_casi);
+
+        for (let n = 0; n < italy.features.length; n++) {
+
+            let jsonState = italy.features[n].properties.NOME_PRO;
+            if (dataState == jsonState) {
+                italy.features[n].properties.value = dataValue;
+                break;
+            }
+        }
+    }
+    g.selectAll("path")
+        .data(italy.features)
         .enter()
-        .append('circle')
-        .attr('cx', d => projection([d.long, d.lat])[0])
-        .attr('cy', d => projection([d.long, d.lat])[1])
-        .style('fill', 'none')
-        .style('stroke', 'tan')
-        .attr('r', d => d.totale_casi / 100)
+        .append("path")
+        .attr("d", path)
+        .style("fill", function (d) {
+            var value = d.properties.value;
+            if (value) {
+                return color(value);
+            } else {
+                return "#f9f9f9"
+            }
+
+        });
+
+    // g.selectAll('circle')
+    //     .data(filteredData)
+    //     .enter()
+    //     .append('circle')
+    //     .attr('cx', d => projection([d.long, d.lat])[0])
+    //     .attr('cy', d => projection([d.long, d.lat])[1])
+    //     .style('fill', 'none')
+    //     .style('stroke', 'tan')
+    //     .attr('r', d => d.totale_casi / 100)
 
     g.selectAll('text')
         .data(filteredData)
@@ -72,7 +146,7 @@ async function loadMarkers(features, data) {
         .append("text")
         .attr('dx', d => projection([d.long, d.lat])[0])
         .attr('dy', d => projection([d.long, d.lat])[1])
-        .style('fill', 'tan')
+        .style('fill', 'black')
         .style("font-size", "10px")
         .text(function (d){
             if (d.totale_casi > 1000){
@@ -80,19 +154,25 @@ async function loadMarkers(features, data) {
             }
         });
 
+
 }
 
 (async () => {
     await loadMap();
     const geojson = await loadData('province');
-    loadMarkers(geojson,"2020-03-22");
+    loadMarkers(geojson, "2020-03-22");
+    console.log(geojson)
 
     globalThis.$('.date input').change(async function () {
         console.log(this.value)
         globalThis.d3.selectAll('circle').remove()
         globalThis.d3.selectAll('text').remove()
+        globalThis.d3.selectAll('path').remove()
+        globalThis.d3.selectAll('rect').remove()
 
         loadMarkers(geojson, this.value);
 
     });
 })();
+
+
