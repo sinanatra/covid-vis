60d09119b36b71f8db3b5b7eb98c7f91362f89f5
Giacomo Nanni <giacomo.nanni@chialab.it>
2020-04-06 15:23:33 +0200

    fix: goddamn csv

--- assets/maps/data.js
+++ assets/maps/data.js
@@ -6,21 +6,21 @@ export function loadData(data) {
     return globalThis.d3.json('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-' + data + '.json');
 }
 
-export function loadDataFrance() {
-    return globalThis.d3.json('https://raw.githubusercontent.com/opencovid19-fr/data/master/dist/chiffres-cles.json');
-}
+// export function loadDataFrance() {
+//     return globalThis.d3.json('https://raw.githubusercontent.com/opencovid19-fr/data/master/dist/chiffres-cles.json');
+// }
 
-export function loadDataGermany() {
-    return globalThis.d3.csv('https://interaktiv.morgenpost.de/corona-virus-karte-infektionen-deutschland-weltweit/data/Coronavirus.regio.v2.csv');
-}
+// export function loadDataGermany() {
+//     return globalThis.d3.csv('https://interaktiv.morgenpost.de/corona-virus-karte-infektionen-deutschland-weltweit/data/Coronavirus.regio.v2.csv');
+// }
 
-export function loadDataAustria() {
-    return globalThis.d3.csv('https://raw.githubusercontent.com/Ramblurr/Austria-COVID-19/master/data/cases.csv');
-}
+// export function loadDataAustria() {
+//     return globalThis.d3.csv('https://raw.githubusercontent.com/Ramblurr/Austria-COVID-19/master/data/cases.csv');
+// }
 
-export function loadDataSwitzerland() {
-    return globalThis.d3.csv('https://raw.githubusercontent.com/covid19-eu-zh/covid19-eu-data/master/dataset/covid-19-ch.csv');
-}
+// export function loadDataSwitzerland() {
+//     return globalThis.d3.csv('https://raw.githubusercontent.com/covid19-eu-zh/covid19-eu-data/master/dataset/covid-19-ch.csv');
+// }
 
 export function loadChart() {
     return globalThis.d3.json('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json');
